659f31e8cee220072e29b40b7c576c7cec918fe0
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2022-09-27 14:32:08 +0200

    Add books through a form

--- src/main/java/nl/miwgroningen/se/ch9/advanced/vincent/libraryDemo/controller/BookController.java
+++ src/main/java/nl/miwgroningen/se/ch9/advanced/vincent/libraryDemo/controller/BookController.java
@@ -1,9 +1,13 @@
 package nl.miwgroningen.se.ch9.advanced.vincent.libraryDemo.controller;
 
+import nl.miwgroningen.se.ch9.advanced.vincent.libraryDemo.model.Book;
 import nl.miwgroningen.se.ch9.advanced.vincent.libraryDemo.repository.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
@@ -19,10 +23,23 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @GetMapping("/books")
-
+    @GetMapping({"/", "/book/overview"})
     protected String showBookOverview(Model model) {
         model.addAttribute("allBooks", bookRepository.findAll());
         return "bookOverview";
+    }
+
+    @GetMapping("/book/new")
+    protected String showBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "bookForm";
+    }
+
+    @PostMapping("/book/new")
+    protected String saveOrUpdateBook(@ModelAttribute("book") Book book, BindingResult result) {
+        if (!result.hasErrors()) {
+            bookRepository.save(book);
+        }
+        return "redirect:/book/overview";
     }
 }
